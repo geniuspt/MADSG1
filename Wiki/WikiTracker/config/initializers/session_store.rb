8557569ec3ae769be7b34ea5e94ff89d1f58f060
joaocorreia <joao.o.correia@gmail.com>
2011-11-17 10:41:17 +0000

    Correcao de erro

--- Wiki/WikiTracker/config/initializers/session_store.rb
+++ Wiki/WikiTracker/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WikiTracker::Application.config.session_store :cookie_store, key: '_WeakyTracker_session'
+WikiTracker::Application.config.session_store :cookie_store, key: '_WikiTracker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
